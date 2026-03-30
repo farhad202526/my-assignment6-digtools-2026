ebae1f1108dc20da1a3897791d2b4d0102e74d85
farhad202526 <fa99.official@gmail.com>
2026-03-30 13:03:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <div>
+      <h1 className='text-center  mt-11 text-red-500 font-bold text-3xl'>Add New One Great on taill add </h1>
+      <h1 className='text-2xl justify-center text-center'>the test one </h1>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
